878157ae3ae1c5d835f6207f9cb527e8797a02c7
2006abhi28 <abhinayasriboddu287@gmail.com>
2025-08-16 23:40:53 +0530

    Fix deployment issues: update build script, settings, and dependencies

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Create a virtual environment
-python3 -m venv venv
-
-# Activate the virtual environment
-source venv/bin/activate
-
 # Upgrade pip
 python -m pip install --upgrade pip
 
 # Install requirements
 pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
 
 # Print Python version for debugging
 python --version
--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'v3v5vfsn0xxjtmb=eoawoiw$5br4g0r&jy_l39995h_93l+-z5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["predico-aditya.onrender.com",
+ALLOWED_HOSTS = ["predico-disease-prediction.onrender.com",
     "127.0.0.1",  # Keep localhost for local development
     "localhost"]
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -82,12 +83,8 @@ WSGI_APPLICATION = 'disease_prediction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'predico',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -129,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-
     os.path.join(BASE_DIR,'templates')
 ]
 
