cf69e4c0c3ac145996f61f435210afef84d5d682
2006abhi28 <abhinayasriboddu287@gmail.com>
2025-08-17 00:06:59 +0530

    Add database migrations to build script

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ python -m pip install --upgrade pip
 # Install requirements
 pip install -r requirements.txt
 
+# Run database migrations
+python manage.py migrate
+
 # Collect static files
 python manage.py collectstatic --noinput
 
